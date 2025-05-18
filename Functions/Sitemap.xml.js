388afe4a09a6cb83818e30510bdc04b8464747df
anticheat131 <wowsamowow@gmail.com>
2025-05-18 14:46:51 +0200

    Update Sitemap.xml.js

--- Functions/Sitemap.xml.js
+++ Functions/Sitemap.xml.js
@@ -1,25 +1,7 @@
-export async function onRequestGet(context) {
-  const domain = 'https://fmovies4u.com';
-  const urls = [];
-
-  for (let i = 0; i < 1000; i++) {
-    urls.push(`${domain}/movie/${100000 + i}`);
-    urls.push(`${domain}/tv/${200000 + i}`);
-  }
-
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls.map(url => `
-  <url>
-    <loc>${url}</loc>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>`).join('')}
-</urlset>`;
-
-  return new Response(sitemap, {
+export async function onRequestGet() {
+  return new Response("Hello from sitemap.xml!", {
     headers: {
-      'Content-Type': 'application/xml'
+      'Content-Type': 'text/plain'
     }
   });
 }
