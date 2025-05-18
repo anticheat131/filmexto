67718df8c46ba7836d8c6ff0b799d676fd9e0fd0
anticheat131 <wowsamowow@gmail.com>
2025-05-18 14:15:16 +0200

    Create generate-sitemap.js

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+const DOMAIN = 'https://fmovies4u.com';
+
+const generateSitemap = () => {
+  const urls = [];
+
+  for (let i = 0; i < 1000; i++) {
+    urls.push(`${DOMAIN}/movie/${100000 + i}`);
+    urls.push(`${DOMAIN}/tv/${200000 + i}`);
+  }
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls
+  .map(
+    (url) => `
+  <url>
+    <loc>${url}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`
+  )
+  .join('')}
+</urlset>`;
+
+  const filePath = path.join(process.cwd(), 'public', 'sitemap.xml');
+  fs.writeFileSync(filePath, xml, 'utf8');
+  console.log('✅ Sitemap generated at public/sitemap.xml');
+};
+
+generateSitemap();
